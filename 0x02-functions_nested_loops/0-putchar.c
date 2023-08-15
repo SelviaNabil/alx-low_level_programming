d3709156aefc2b29b07e6e0e16ed973c4b5bf582
Your Name <you@example.com>
2023-08-15 14:40:32 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,27 @@
-#!/bin/bash
 /**
  * C - Functions, nested loops
  * auth :Selvia Nabil
  */
-#include <stdlib.h>
+
 #include <main.h>
-#include <stdio.h>
+
 
 /**
- * main - prints _putchar,
- *  followed by a new line.
- *
- * Return: Always 0.
+ * main - print _putchar
+ * is written by selvia
+ * Return: 0
 */
 
 int main(void)
 {
-	echo '_putchar' \n
-return0 ;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
