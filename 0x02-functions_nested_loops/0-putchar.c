43d60f6e886ffc3f7d3c30360521b185e9ff7bcf
Your Name <you@example.com>
2023-08-15 05:16:05 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,18 @@
 #!/bin/bash
+/**
+ * C - Functions, nested loops
+ * auth :Selvia Nabil
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints _putchar,
+ *  followed by a new line.
+ *
+ * Return: Always 0.
+*/
+
 echo '_putchar' \n
 return 0
