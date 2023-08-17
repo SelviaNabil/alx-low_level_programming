552b84b761ca0c9567274ef45e1482e1b2e97c15
Your Name <you@example.com>
2023-08-17 11:00:39 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(char c)
+{
+if (c >= 65 && c <= 90)
+{
+	return (1);
+	}
+	else
+       {
+		return (0);
+	}
+
+}
+int main(void)
+{
+	char c;
+c = 'A';
+int a;
+
+a = _isupper(c);
+
+printf("%c: %d\n", c, a);
+
+c = 'a';
+
+printf("%c: %d\n", c, a);
+
+return (0);
+
+}
