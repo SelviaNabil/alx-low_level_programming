db12128353e6d4371b42976710ae10ba72442fa4
Your Name <you@example.com>
2023-08-18 04:43:02 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 
 /**
  * print_triangle - print a triangle
@@ -9,6 +9,7 @@
 
 void print_triangle(int size)
 int height, base;
+
 if (size <= 0)
 {
 _putchar('\n');
