dbdf34e0c502444edb6546fb557d23d769f8691d
Your Name <you@example.com>
2023-08-17 19:55:12 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include 'main.h'
+
+/**
+ * print_triangle - print a triangle
+ * @size: size of triangle
+ *
+ * Return: always success 0
+ */
+
+void print_triangle(int size)
+int height, base;
+if (size <= 0)
+{
+_putchar('\n');
+else
+for (height = 1; base <= size; base++);
+{
+if((height+base) <= size)
+{
+_putchar(' ');
+else
+_putchar('#');
+}
+}
+}
