fdfc8b7b8629b5ce5874f3b2176656df3749a04c
Your Name <you@example.com>
2023-08-17 19:11:08 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,11 @@
  *@c: tested character
  *Return: returns 1 if it is uppercase , 0 if not
  */
-int _isdigit(int c);
+int _isdigit(int c)
 if ((c >= 48) && (c <= 57))
 {
-	return (1);
-}
+return (1);
+
 else
-{
-	return (0);
+return (0);
 }
