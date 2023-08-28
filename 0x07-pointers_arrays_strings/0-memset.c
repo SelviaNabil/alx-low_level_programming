b0861cd1c912fd3c28e41da1fe234f3f5871b4ea
Your Name <you@example.com>
2023-08-28 04:22:29 -0700

    7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _memset - fills memory with a constant byte.
  * @s: source string
@@ -8,12 +8,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
+unsigned int i = 0;
 
-for (; n > 0; i++)
+while (i < n)
 {
-s[i] = b;
-n--;
+*(s + i) = b;
+i++;
 }
 return (s);
 }
