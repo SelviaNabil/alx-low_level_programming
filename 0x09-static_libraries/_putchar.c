8c43931f19483b49dfa3412c0262f98287a4504e
Your Name <you@example.com>
2023-09-01 15:30:51 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar : write the character C to stdout
+ * @C : the character to print
+ *
+ * Return : on success 
+ * On error, -1 is returned, and ermo is set appropniately
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
